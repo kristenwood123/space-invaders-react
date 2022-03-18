6d9f215c5801f7fe7421f414aa327248cc1395a3
Kristen <kristenwood123@gmail.com>
2022-03-17 22:41:41 -0700

    adjusted aliens

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import { numbers } from "./data";
 
 function App() {
   const [squares, setSquares] = useState(numbers);
-  const currentShooterIndex = 202;
+  const currentShooterIndex = 187;
   const alienInvaders = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30,
-    31, 32, 33, 34, 35, 36, 37, 38, 39,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
   ];
 
   return (
     <div className="grid">
-      {alienInvaders.map((i, index) => {
-        return <span key={index} className="invader"></span>;
-      })}
+      <div className="invader-container">
+        {alienInvaders.map((i, index) => {
+          return <div key={index} className="invader"></div>;
+        })}
+      </div>
       {squares.map((i, index) => {
         return (
           <div
