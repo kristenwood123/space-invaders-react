c224c72911662d7b8e35d1f82fbae428d53cb732
Kristen <kristenwood123@gmail.com>
2022-03-17 22:30:12 -0700

    added shooter

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import { numbers } from "./data";
 
 function App() {
   const [squares, setSquares] = useState(numbers);
-  const [isInvader, setIsInvader] = useState(true);
-
+  const currentShooterIndex = 202;
   const alienInvaders = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30,
     31, 32, 33, 34, 35, 36, 37, 38, 39,
   ];
-
-  const isFound = (squares) => {
-    const result = squares.filter((num) => num === 0);
-
-    if (result.length === 1) return setIsInvader(true);
-    return false;
-  };
-  isFound(squares);
 
   return (
     <div className="grid">
@@ -25,7 +16,12 @@ function App() {
         return <span key={index} className="invader"></span>;
       })}
       {squares.map((i, index) => {
-        return <div key={i} className="sq"></div>;
+        return (
+          <div
+            key={i}
+            className={index === currentShooterIndex ? "shooter" : "square"}
+          ></div>
+        );
       })}
     </div>
   );
