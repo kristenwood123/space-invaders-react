fab25f36b82fa99a411bc34926b328d6bc886ed7
Kristen <kristenwood123@gmail.com>
2022-03-17 23:31:04 -0700

    can move shooter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./styles.css";
 import { numbers } from "./data";
 
 function App() {
   const [squares, setSquares] = useState(numbers);
-  const currentShooterIndex = 177;
+  let [currentShooterIndex, setCurrentShooterIndex] = useState(177);
+
   const alienInvaders = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30,
     31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
   ];
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowLeft") {
+        setCurrentShooterIndex((currentShooterIndex -= 1));
+        console.log(currentShooterIndex);
+      }
+      if (e.key === "ArrowRight") {
+        setCurrentShooterIndex((currentShooterIndex += 1));
+        console.log(currentShooterIndex);
+      }
+    });
+  }, []);
 
   return (
     <div className="container">
