1ad5f22b2f4b511cc11b3e3dbfe9bddbc904991d
Kristen <kristenwood123@gmail.com>
2022-03-17 22:46:19 -0700

    added padding on top

--- src/App.js
+++ src/App.js
@@ -4,27 +4,29 @@ import { numbers } from "./data";
 
 function App() {
   const [squares, setSquares] = useState(numbers);
-  const currentShooterIndex = 187;
+  const currentShooterIndex = 177;
   const alienInvaders = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30,
     31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
   ];
 
   return (
-    <div className="grid">
-      <div className="invader-container">
-        {alienInvaders.map((i, index) => {
-          return <div key={index} className="invader"></div>;
+    <div className="container">
+      <div className="grid">
+        <div className="invader-container">
+          {alienInvaders.map((i, index) => {
+            return <div key={index} className="invaders"></div>;
+          })}
+        </div>
+        {squares.map((i, index) => {
+          return (
+            <div
+              key={i}
+              className={index === currentShooterIndex ? "shooter" : "square"}
+            ></div>
+          );
         })}
       </div>
-      {squares.map((i, index) => {
-        return (
-          <div
-            key={i}
-            className={index === currentShooterIndex ? "shooter" : "square"}
-          ></div>
-        );
-      })}
     </div>
   );
 }
