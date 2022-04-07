98eca64e89cede0e4db136445d05ec2bcce829a5
Kristen <kristenwood123@gmail.com>
2022-04-07 10:47:11 -0700

    added styles

--- src/styles/StyledGrid.js
+++ src/styles/StyledGrid.js
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
 export const StyledGrid = styled.div`
-display: grid;
-grid-template-rows: repeat(
-  ${(props) => props.height},
-  calc(25vw / ${(props) => props.width})
-)
-grid-template-columns: repeat(${(props) => props.width}, 1fr);
-grid-gap: 1px;
-border: 1px solid white;
-width: 100%;
-max-width: 25vw;
-background: black`;
+  display: grid;
+  grid-template-rows: repeat(
+    ${(props) => props.height},
+    calc(25vw / ${(props) => props.width})
+  );
+  grid-template-columns: repeat(${(props) => props.width}, 1fr);
+  grid-gap: 1px;
+  border: 1px solid white;
+  width: 100%;
+  max-width: 25vw;
+  background: #111;
+`;
+
+export default StyledGrid;
